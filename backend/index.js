37f809ffadfe9a8fafe83475373938c6d4d1cfeb
Tezivindh <tezivindh@gmail.com>
2025-04-21 01:26:19 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoutes from "./src/routes/auth.route.js";
+import messageRoutes from "./src/routes/message.route.js";
+
+import { connectDB } from "./src/lib/db.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+app.listen(PORT, () => {
+  console.log("server listening from port:" + PORT);
+  connectDB();
+});
