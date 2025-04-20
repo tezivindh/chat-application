75cbe3ba6293b46c0901ccddc5f2135e9a369450
Tezivindh <tezivindh@gmail.com>
2025-04-21 02:22:22 +0530

    Fixed:Sharing Images

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: "15mb" }));
+app.use(express.urlencoded({ extended: true, limit: "15mb" }));
 app.use(cookieParser());
 app.use(
   cors({
