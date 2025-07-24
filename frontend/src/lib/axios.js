5265cff6af3301c6d8a37b01a3c70bda0adaa2d0
tezivindh <tezivindh@gmail.com>
2025-07-24 23:21:28 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/app/api"
-      : "/app/api",
+      : "https://chat-application-kappa-nine.vercel.app/app/api",
   withCredentials: true,
 });
